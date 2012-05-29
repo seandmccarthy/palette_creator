b8f2011dbc9ed8113179727cad6a7180f90c1cad
Sean McCarthy <sean@clanmccarthy.net>
2012-05-29 22:17:24 +1000

    Initial version

--- Rakefile
+++ Rakefile
@@ -17,8 +17,8 @@ Jeweler::Tasks.new do |gem|
   gem.name = "palette_creator"
   gem.homepage = "http://github.com/seandmccarthy/palette_creator"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
+  gem.summary = %Q{Create rainbow colour palettes suitable for graphs}
+  gem.description = %Q{Create rainbow colour palettes suitable for graphs}
   gem.email = "sean@clanmccarthy.net"
   gem.authors = ["Sean McCarthy"]
   # dependencies defined in Gemfile
---
+++ lib/palette_creator.rb
@@ -0,0 +1,38 @@
+require 'ostruct'
+
+# Author::    Sean McCarthy
+# Copyright:: Copyright (c) 2012 Sean McCarthy
+# License::   GPL3
+#
+# Based on http://krazydad.com/tutorials/makecolors.php
+class PaletteCreator
+
+  # Create a colour palette
+  #
+  # _options_ can have the following keys: 
+  # * *count*: number of colours to generate
+  # * *frequency*: frequency of the sine wave
+  # * *phases*: 
+  def self.create(options={})
+    opts = OpenStruct.new(options)
+    count     = opts.count || 32
+    frequency = opts.frequency || 0.3
+    phases    = opts.phases || [0, 2, 4]
+    amplitude = 127 # ~ 255/2
+    center    = 128 # ~ 255/2
+
+    palette = []
+    count.times do |i|
+      red   = Math.sin(frequency*i + phases[0]) * amplitude + center;
+      green = Math.sin(frequency*i + phases[1]) * amplitude + center;
+      blue  = Math.sin(frequency*i + phases[2]) * amplitude + center;
+      palette << [red.to_i, green.to_i, blue.to_i]
+    end
+    palette
+  end
+
+  def self.rgb_as_hex(rgb_array)
+    rgb_array.map{|val| "%02x" % val}.join('').upcase
+  end
+
+end
